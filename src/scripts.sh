ac591e33cccb90a0682592d9508a0530486dce55
Jy Kingston <jy@buildkite.com>
2024-10-10 16:24:26 +1100

    wip

--- src/scripts.sh
+++ src/scripts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
 IMAGE_TO_DEPLOY="This is rajat"
-buildkite-agent meta-data set "my_image" "${IMAGE_TO_DEPLOY}"
-buildkite-agent meta-data get "my_image"
+buildkite-agent meta-data set "my_image" "$IMAGE_TO_DEPLOY"
+CLOUD_RUN_IMAGE=$(buildkite-agent meta-data get "my_image")
+echo $CLOUD_RUN_IMAGE
