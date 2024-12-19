5a2639e492a5a4dee8193dc8c00de15fc5824552
Jy Kingston <jy@buildkite.com>
2024-12-19 16:15:02 +1100

    wip

--- src/env-vars.sh
+++ src/env-vars.sh
@@ -12,7 +12,7 @@ env_vars_array+=("$env_vars")
 # Append additional environment variables to the array
 env_vars_array+=(
   "BUILDKITE_AGENT_DEBUG=\"null\""  # Explicitly set to "null"
-  "BUILDKITE_BUILD_PATH=${BUILDKITE_BUILD_PATH_SUBAGENT:-$BUILDKITE_BUILD_PATH}"
+  "BUILDKITE_BUILD_PATH=\"${BUILDKITE_BUILD_PATH_SUBAGENT:-$BUILDKITE_BUILD_PATH}\""
 )
 
 # Output the variables
@@ -20,3 +20,6 @@ echo "Output:"
 for var in "${env_vars_array[@]}"; do
     echo "$var"
 done
+
+
+zsh -c "echo ${env_vars[@]}"
