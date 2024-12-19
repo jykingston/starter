d1c776b5c61ad368d6a29f5e146af3141dd9c79d
Jy Kingston <jy@buildkite.com>
2024-12-19 16:06:26 +1100

    wip

--- src/env-vars.sh
+++ src/env-vars.sh
@@ -4,31 +4,26 @@ set -euo pipefail
 echo "Importing the env vars"
 env_vars=$(<"$BUILDKITE_ENV_FILE")
 
-echo "Output them for posterity"
-echo $env_vars
+declare -a env_vars_array
 
-env_vars+=(
-    BUILDKITE_AGENT_ACCESS_TOKEN=""
-    BUILDKITE_BUILD_PATH=""
-    BUILDKITE_HOOKS_PATH=""
-    BUILDKITE_PLUGINS_PATH=""
+# Add existing environment variables
+env_vars_array+=("$env_vars")
+
+# Append additional environment variables to the array
+env_vars_array+=(
+    BUILDKITE_AGENT_ACCESS_TOKEN=${BUILDKITE_AGENT_ACCESS_TOKEN_SUBAGENT:-$BUILDKITE_AGENT_ACCESS_TOKEN}
+    BUILDKITE_BUILD_PATH=${BUILDKITE_BUILD_PATH_SUBAGENT:-$BUILDKITE_BUILD_PATH}
+    BUILDKITE_HOOKS_PATH=${BUILDKITE_HOOKS_PATH_SUBAGENT:-$BUILDKITE_HOOKS_PATH}
+    BUILDKITE_PLUGINS_PATH=${BUILDKITE_PLUGINS_PATH_SUBAGENT:-$BUILDKITE_PLUGINS_PATH}
+    BUILDKITE_PLUGINS_ENABLED=${BUILDKITE_PLUGINS_ENABLED_SUBAGENT:-$BUILDKITE_PLUGINS_ENABLED}
+    BUILDKITE_PLUGIN_VALIDATION=${BUILDKITE_PLUGIN_VALIDATION_SUBAGENT:-$BUILDKITE_PLUGIN_VALIDATION}
+    BUILDKITE_LOCAL_HOOKS_ENABLED=${BUILDKITE_LOCAL_HOOKS_ENABLED_SUBAGENT:-$BUILDKITE_LOCAL_HOOKS_ENABLED}
+    BUILDKITE_SSH_KEYSCAN=${BUILDKITE_SSH_KEYSCAN_SUBAGENT:-$BUILDKITE_SSH_KEYSCAN}
+    BUILDKITE_AGENT_DEBUG=${BUILDKITE_AGENT_DEBUG_SUBAGENT:-$BUILDKITE_AGENT_DEBUG}
 )
 
-echo "output with formatting lah"
-echo
-echo ${env_vars[@]}
-echo
-echo "Print without formatting"
-echo $env_vars
-echo
-echo "with zsh"
-zsh -c "echo \"$env_vars\""
-#for var in "${env_vars[@]}"; do
-#  echo "$var"
-#done
-#env_vars="${env_vars[@]}"  # Correctly combine the array into a single string
-
-#echo
-#echo "testing with zsh"
-#escaped_vars=$(printf '%q ' "${env_vars[@]}")
-#zsh -c "echo \"$escaped_vars\""
+# Output the variables
+echo "Output:"
+for var in "${env_vars_array[@]}"; do
+    echo "$var"
+done
