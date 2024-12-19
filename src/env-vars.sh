9df4f8f6f4587cd8f44fd44ca5343d28677e54d5
Jy Kingston <jy@buildkite.com>
2024-12-19 16:30:40 +1100

    wip

--- src/env-vars.sh
+++ src/env-vars.sh
@@ -4,22 +4,19 @@ set -euo pipefail
 echo "Importing the env vars"
 env_vars=$(<"$BUILDKITE_ENV_FILE")
 
+# Declare the escaped array (forced quotes)
 declare -a env_vars_array
 
 # Add existing environment variables
 env_vars_array+=("$env_vars")
 
-# Append additional environment variables to the array
-env_vars_array+=(
-  "BUILDKITE_AGENT_DEBUG=\"null\""  # Explicitly set to "null"
-  "BUILDKITE_BUILD_PATH=\"${BUILDKITE_BUILD_PATH_SUBAGENT:-$BUILDKITE_BUILD_PATH}\""
+# Append additional environment variables to the array (subset)
+env_vars_array+=( 
+  "BUILDKITE_BUILD_PATH=\"${BUILDKITE_BUILD_PATH_SUBAGENT:-$BUILDKITE_BUILD_PATH}\"" # This is correctly escaped
 )
 
 # Output the variables
-echo "Output:"
-#for var in "${env_vars_array[@]}"; do
-#    echo "$var"
-#done
-
-
-zsh -c "echo \"${env_vars[@]}\""
+echo "Output BASH"
+bash -c "echo \"${env_vars_array[@]}\""
+echo "Output ZSH"
+zsh -c "echo \"${env_vars_array[@]}\""
