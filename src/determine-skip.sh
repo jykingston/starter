22f63faf41bc94e895d23a28cfdead27429ea435
Jy Kingston <jy@buildkite.com>
2025-02-12 11:23:09 +1100

    Fix sytax

--- src/determine-skip.sh
+++ src/determine-skip.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# export SKIP_TESTS=false
-export SKIP_TESTS=true
+export SKIP_TESTS=false
+#export SKIP_TESTS=true
 echo "Skip Tests = $SKIP_TESTS"
 
 # Run agent pipeline upload for the step if it didn't Skip
 if [ "$SKIP_TESTS" = false ]; then
     echo "Run Tests"
-    buildkite-agent pipeline upload .buildkite/pipeline.yml >> buildkite-env.sh
+    echo "buildkite-agent pipeline upload .buildkite/pipeline.yml" >> buildkite-env.sh
 else
     echo "Skip Tests"
-    buildkite-agent pipeline upload .buildkite/pipeline-skip.yml >> buildkite-env.sh
+    echo "buildkite-agent pipeline upload .buildkite/pipeline-skip.yml" >> buildkite-env.sh
 fi
