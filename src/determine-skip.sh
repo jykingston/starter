f8987be218281ce8a8ea662de32bbc87001df5e3
Jy Kingston <jy@buildkite.com>
2025-02-12 11:32:25 +1100

    Try that again now

--- src/determine-skip.sh
+++ src/determine-skip.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-#export SKIP_TESTS=false
-export SKIP_TESTS=true
-echo "Skip Tests = $SKIP_TESTS"
-
-# Run agent pipeline upload for the step if it didn't Skip
-if [ "$SKIP_TESTS" = false ]; then
-    echo "Run Tests"
-    echo "buildkite-agent pipeline upload .buildkite/pipeline.yml" >> buildkite-env.sh
-else
-    echo "Skip Tests"
-    echo "buildkite-agent pipeline upload .buildkite/pipeline-skip.yml" >> buildkite-env.sh
-fi
+echo "true"
